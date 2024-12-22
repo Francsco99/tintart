15c89b86b68a418321a68efeaa3ac361214e438e
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-12-22 22:49:13 +0100

    testo smoot index

---
+++ assets/js/smooth_text.js
@@ -0,0 +1,13 @@
+$(function () {
+    // Seleziona l'elemento service
+    $('.service').hover(
+      function () {
+        // Quando il mouse entra, mostra il testo con fadeToggle
+        $(this).find('.service-text').fadeIn(600);
+      },
+      function () {
+        // Quando il mouse esce, nascondi il testo con fadeToggle
+        $(this).find('.service-text').fadeOut(400);
+      }
+    );
+  });
