ed8f830edfaf2daaf1f1e04c8a05545701480ef2
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-12-23 11:15:49 +0100

    lavoro pagina

---
+++ assets/js/changePhoto.js
@@ -0,0 +1,8 @@
+function changePhoto(photoSrc) {
+    const largeImage = document.getElementById("largeImage");
+    largeImage.style.opacity = 0; // Inizia la dissolvenza
+    setTimeout(() => {
+        largeImage.src = photoSrc;
+        largeImage.style.opacity = 1; // Ripristina l'opacità
+    }, 300); // Durata della dissolvenza deve coincidere con la durata della transizione
+  }
